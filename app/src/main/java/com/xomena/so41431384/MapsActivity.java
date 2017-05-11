b5dbee4dda000b2fffee64147c4e6d1f8d75e7ee
xomena-so <geo.emea.sbt@gmail.com>
2017-05-11 16:16:32 +0200

    initial commit

---
+++ app/src/main/java/com/xomena/so41431384/MapsActivity.java
@@ -0,0 +1,73 @@
+package com.xomena.so41431384;
+
+import android.graphics.Color;
+import android.support.v4.app.FragmentActivity;
+import android.os.Bundle;
+import android.util.Log;
+
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.maps.android.data.geojson.GeoJsonLayer;
+import com.google.maps.android.data.geojson.GeoJsonPolygonStyle;
+
+import org.json.JSONException;
+
+import java.io.IOException;
+
+public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
+
+    private GoogleMap mMap;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_maps);
+        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
+    }
+
+
+    /**
+     * Manipulates the map once available.
+     * This callback is triggered when the map is ready to be used.
+     * This is where we can add markers or lines, add listeners or move the camera. In this case,
+     * we just add a marker near Sydney, Australia.
+     * If Google Play services is not installed on the device, the user will be prompted to install
+     * it inside the SupportMapFragment. This method will only be triggered once the user has
+     * installed Google Play services and returned to the app.
+     */
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        mMap = googleMap;
+
+        mMap.getUiSettings().setZoomControlsEnabled(true);
+
+        // Add a marker in Sydney and move the camera
+        LatLng madrid = new LatLng(40.416775,-3.70379);
+        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(madrid, 3F));
+
+        try {
+            GeoJsonLayer layer = new GeoJsonLayer(mMap, R.raw.es_geojson, getApplicationContext());
+
+            GeoJsonPolygonStyle style = layer.getDefaultPolygonStyle();
+            style.setFillColor(Color.MAGENTA);
+            style.setStrokeColor(Color.MAGENTA);
+            style.setStrokeWidth(1F);
+
+            layer.addLayerToMap();
+
+        } catch (IOException ex) {
+            Log.e("IOException", ex.getLocalizedMessage());
+        } catch (JSONException ex) {
+            Log.e("JSONException", ex.getLocalizedMessage());
+        }
+
+
+    }
+}
